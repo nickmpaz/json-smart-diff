f647c6f2cd443e90b7ff359c5336de41f5f5d8e7
Nick Paz <nicholasmpaz@gmail.com>
2020-01-24 09:47:36 -0800

    code review

--- json_compare/json_compare.py
+++ json_compare/json_compare.py
@@ -2,22 +2,38 @@ import json
 import sys
 import tabulate
 
+# FIXME GENERAL STUFF
+
+# python naming conventions dont like uppercase for methods or variables
+# use_snake_case_instead
+
+# consider using more descriptive variable names instead of things like 'x'
+
+# FIXME
+# error handling for not enough args
+if len(sys.argv) > 3:
+    print('usage: print a help string here')
+    sys.exit()
+
 file1 = sys.argv[1]
 file2 = sys.argv[2]
 
-        
 def flatten_json(y):
     out = {}
 
     def flatten(x, name=''):
+        
+        
         if type(x) is dict:
+        
             for a in x:
                 flatten(x[a], name + a + '_')
         elif type(x) is list:
-            i = 0
-            for a in x:
+
+            # FIXME for this loops consider using the 'enumerate' builtin method.
+            # enumerate is insanely useful and comes up alot
+            for i, a in enumerate(x):
                 flatten(a, name + str(i) + '_')
-                i += 1
         else:
             out[name[:-1]] = x
 
@@ -28,8 +44,11 @@ def flatten_json(y):
 def processFile(file):
     with open(file,'r') as fileData:
         fileString = fileData.read()
-        fileDictionary = json.loads(fileString)
-        return flatten_json(fileDictionary)
+
+    # FIXME convention says only put the lines that NEED the file inside the 'with' statement
+    # these two lines don't need to be in the with statement
+    fileDictionary = json.loads(fileString)
+    return flatten_json(fileDictionary)
 
 
 def compareKeys(dict1, dict2):
@@ -48,14 +67,18 @@ def compareKeys(dict1, dict2):
 
 
 def printUniqueKeys(uniqueKeys):
-    if len(uniqueKeys[0]) == 0 & len(uniqueKeys[1]) == 0:
+    # FIXME generally shouldn't use bitwise '&' 
+    # instead use python keyword 'and'
+    if len(uniqueKeys[0]) == 0 and len(uniqueKeys[1]) == 0:
         print('\n%s and %s have identical keys!' %(file1,file2))
         return
     print("\nTable of unique keys:\n")
     rows = max(len(uniqueKeys[0]),len(uniqueKeys[1]))
     keyList = []
 
-    for i in range(0,rows):
+    # FIXME if you want range to begin at 0, you don't need to put the 0
+    # range(0, 5) is the same as range(5)
+    for i in range(rows):
         emptyArr = [None]*2
 
         try:
